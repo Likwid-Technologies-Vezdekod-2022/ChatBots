f5dc5ab6ee73747911cc84b0768ed36ba138ad2c
Алексей <alexleskov2000@gmail.com>
2022-06-04 17:50:03 +0800

    Модели изображений, игр и коллекций

--- src/vk_bot/admin.py
+++ src/vk_bot/admin.py
@@ -8,3 +8,21 @@ class VkUserAdmin(admin.ModelAdmin):
     list_display = ('id', 'chat_id', 'name',)
     search_fields = ('id', 'chat_id', 'name',)
     ordering = ['-update_date']
+
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'standard', 'creation_date',)
+    ordering = ['-update_date']
+
+
+@admin.register(models.Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'collection', 'attachment_data', 'creation_date',)
+    ordering = ['-update_date']
+
+
+@admin.register(models.Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'collection', 'status')
+    ordering = ['-update_date']
--- src/vk_bot/models.py
+++ src/vk_bot/models.py
@@ -5,6 +5,8 @@ class VkUser(models.Model):
     chat_id = models.CharField(max_length=15, verbose_name='chat_id')
     name = models.CharField(max_length=150, verbose_name='Имя', blank=True)
 
+    current_game = models.ForeignKey('Game', on_delete=models.SET_NULL, null=True, blank=True)
+
     creation_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
@@ -15,3 +17,44 @@ class VkUser(models.Model):
 
     def __str__(self):
         return f'{self.chat_id} ({self.name})'
+
+
+class Collection(models.Model):
+    standard = models.BooleanField(default=False)
+    words = models.JSONField()
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Коллекиция'
+        verbose_name_plural = 'Коллекиции'
+        ordering = ['-update_date']
+
+
+class Image(models.Model):
+    collection = models.ForeignKey('Collection', on_delete=models.CASCADE, related_name='images')
+
+    attachment_data = models.CharField(max_length=1000)
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Изображение'
+        verbose_name_plural = 'Изображения'
+        ordering = ['-update_date']
+
+
+class Game(models.Model):
+    collection = models.ForeignKey('Collection', on_delete=models.CASCADE)
+    status = models.CharField(choices=[('created', 'created'), ('started', 'started'), ('finished', 'finished')],
+                              default='created', max_length=400)
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Игра'
+        verbose_name_plural = 'Игры'
+        ordering = ['-update_date']
